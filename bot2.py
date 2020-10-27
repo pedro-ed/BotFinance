f3efd00decbeef23853bb9c446e6b9aa15d93eaf
pedroadv <pedroadv991@gmail.com>
2020-10-27 10:25:44 -0300

    juntar os modulos

--- bot2.py
+++ bot2.py
@@ -1,5 +1,21 @@
+from pyiqoptionapi import IQOption
+import pyiqoptionapi
+import datetime
+import time
+import json
+import logging
+
 from colorama import init,Fore,Back
-from iq import comprar, connect, vender, indexBots, createLog, getCandle,getparidade
+#manipulador do sistema 
+import sys
+#manipulador de tempo
+from datetime import  datetime,timedelta
+from time import time
+import requests
+import json
+api = ''
+from colorama import init,Fore,Back
+# from iq import comprar, connect, vender, indexBots, createLog, getCandle,getparidade
 #Connectar
 import sys
 import threading
@@ -8,20 +24,115 @@ from win10toast import ToastNotifier
 toast = ToastNotifier()
 import os
 import time 
-connect()
-init(autoreset=True)
-from datetime import datetime
+
 PRODUCION = False
 
 if '-P' in sys.argv:
     PRODUCION = True
 
-print(PRODUCION)
+
+def connect():
+    init(autoreset=True)
+    #LOGIN
+    logging.basicConfig(format='%(asctime)s %(message)s')
+    global api
+    api = IQOption("pedroadv991@gmail.com", "pedropedroka")
+    api.connect()
+    if PRODUCION:
+        api.change_balance('REAL')#PRACTICE \ REAL
+    else:
+        api.change_balance('PRACTICE')#PRACTICE \ REAL
+    if api.check_connect():
+        return True
+    else:
+        return False
+
+def comprar(invest,par,timeframe):
+    entrada = invest
+    direcao = 'call'
+    status,id = api.buy(entrada, par, direcao, timeframe)
+    if status:
+        resultado = api.check_win_v3(id)
+        return(resultado)
+    else:
+        return(status)
+
+
+def vender(invest,par,timeframe):
+    entrada = invest
+    direcao = 'put'
+    status,id = api.buy(entrada, par, direcao, timeframe)
+    if status:
+        resultado = api.check_win_v3(id)
+        return(resultado)
+    else:
+        return(status)
+
+
+
+def indexBots():
+    url = "https://apibots-937d3.firebaseio.com/Botmanager/Bots.json"
+    payload = ""
+    response = requests.request("GET", url, data=payload)
+    return json.loads(response.text)    
+
+def createLog(ID_BOT,result,direction,prejuizo,lucro,timeframe,Nexec,investiment,nameBot):
+    url = "https://apibots-937d3.firebaseio.com/Botmanager/Logs.json"
+    headers = {'content-type': 'application/json'}
+    payload = {
+                "createdAt":time(),
+                "ID_Bot":ID_BOT,
+                "nameBot":nameBot,
+                "balanceTotal":api.get_balance(),
+                "result":result,
+                "direction":direction,
+                "prejuizo":prejuizo,
+                "lucro":lucro,
+                "timeFrame":timeframe,
+                "NumExecution":Nexec,
+                "investiment":investiment
+                }
+    response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+    return (response.text)
+
+
+def getCandle(nC,time,timeframe,par):
+    """
+    Captura Candles
+    """
+    return api.get_candles(par,(timeframe * 60),nC,time)
+
+
+
+
+def getparidade():
+    par = api.get_all_open_time()
+    pares = []
+    for paridade in par['turbo']:
+        if par['turbo'][paridade]['open'] == True:
+            pares.append(paridade)
+    return pares
+
+
+def reconnectIQ():
+    api.close_connect()
+    # connect()
 
 
 
 def notfy(title,boby):
     toast.show_toast(title,boby,duration=20,icon_path=r"C:\Users\User\Pictures\Diversity-Avatars-Avatars-Robot-01.ico")
+
+connect()
+init(autoreset=True)
+from datetime import datetime
+
+
+
+
+
+
+
 
 def executeBot(invest,par,direcao,timeframe,id_bot,name):
     i=0
