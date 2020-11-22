e6f96e899162ed5a8fd71f19fb72427d192f3ef9
pedroadv <pedroadv991@gmail.com>
2020-11-22 19:07:54 -0300

    ant

--- botPy/GetPares/__init__.py
+++ botPy/GetPares/__init__.py
@@ -1,7 +1,10 @@
-def index(api):
+def index(api,parAtivo=False):
     par = api.get_all_open_time()
     pares = []
     for paridade in par['turbo']:
-        if par['turbo'][paridade]['open'] == True:
-            pares.append(paridade)        
+        if parAtivo:
+            pares.append(paridade)
+        else:
+            if par['turbo'][paridade]['open'] == True:
+                pares.append(paridade)        
     return pares
---
+++ botPy/ant.py
@@ -0,0 +1,24 @@
+import GetCadle, iqLogin,time,GetPares
+par= 'EURNZD'
+print("Logando")
+api = iqLogin.login()
+print("Pedando Pares")
+pares = GetPares.index(api,parAtivo=True)
+
+
+data =[]
+print("Pegando cadles")
+for par in pares:
+    cadles = api.get_candles(par,1,1000,time.time())
+    data.append([par,cadles])
+
+x = 0
+print("Calculando")
+for item in data:
+    cd = item[1]
+    for i in cd:
+        opem = i['open']
+        close = i['close']
+    x+=1
+
+print(i)
